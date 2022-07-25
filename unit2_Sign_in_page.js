9fee97cbc9a99f6ab83efc84994369edda80503e
Shubham Singh <shubhammk21@gmail.com>
2022-07-25 10:31:08 +0530

    <updade>

--- unit2_Sign_in_page.js
+++ unit2_Sign_in_page.js
@@ -3,25 +3,26 @@ let data=JSON.parse(localStorage.getItem("unit2_project_data")) || [];
 
 form.addEventListener("submit",function(event){
     event.preventDefault();
+    let f=false;
     let dd=document.querySelector("#details");
     data.forEach(function(el){ 
-        if(form.pass2.value!=el.pass2 && form.email.value===el.email){
-            dd.style.backgroundColor="red"
-            document.querySelector("#details >p").innerText="X The Password you entered does not correct. Did you mean to create an account?"
-        }
-        else if(form.pass2.value===el.pass1 && form.email.value===el.email){
+        if(form.pass2.value===el.pass1 && form.email.value===el.email){
             alert("Sucessfully login");
             window.location.href="index.html"
+            f=true
             return;
         }
-        else if(form.email.value==el.email){
+        if(form.pass2.value!=el.pass2 && form.email.value==el.email){
             dd.style.backgroundColor="red"
-            document.querySelector("#details >p").innerText="X The email you entered does not belong to any account. Did you mean to create an account?"
-        }
-        else{
-            dd.style.backgroundColor="red"
-            document.querySelector("#details >p").innerText="X The email you entered does not belong to any account. Did you mean to create an account?"
-        }
+            document.querySelector("#details >p").innerText="X The Password you entered does not correct. Did you mean to create an account?"
+            f=true
+            return;
+        }  
     })
+    if(f==false){
+        dd.style.backgroundColor="red"
+        document.querySelector("#details >p").innerText="X The email you entered does not belong to any account. Did you mean to create an account?"
+        return;
+    }
     
 })
